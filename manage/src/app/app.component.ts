6e9279cfc1303516618a9d84c48e198c3c6f5377
Nikody Keating <nikody.keating@gmail.com>
2025-05-10 07:58:50 -0700

    Adding configuration based path for management project base path

--- manage/src/app/app.component.ts
+++ manage/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
     selector: 'app-root',
@@ -25,4 +26,11 @@ export class AppComponent {
       }
     },
   };
+
+  constructor() {
+    const base = window.document.getElementById('baseHref')
+      if(base) {
+        base.setAttribute('href', environment.routes.behavior);
+      }
+  }
 }
