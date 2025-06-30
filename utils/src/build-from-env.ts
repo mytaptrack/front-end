6a8db0f492fe32f4d45deda096dfa89a57443da4
Nikody Keating <nikody.keating@gmail.com>
2025-06-30 08:37:54 -0700

    Updating manage as well

--- utils/src/build-from-env.ts
+++ utils/src/build-from-env.ts
@@ -5,7 +5,7 @@ function processEnvFile(path: string) {
     let envFileContent = readFileSync(path).toString();
 
     const keys = ['USER_POOL_ID','IDENTITY_POOL_ID','COGNITO_CLIENT_ID', 'AWS_REGION','BEHAVIOR_DOMAIN','API_DOMAIN','GRAPH_DOMAIN','COGNITO_DOMAIN','MANAGE_DOMAIN']
-    for(let key of keys) {
+     for(let key of keys) {
         console.log(key, process.env[key])
 
         const codeEnvName = `process\.env\.${key}`;
@@ -22,11 +22,19 @@ processEnvFile('../behavior/src/environments/environment.prod.ts');
 processEnvFile('../manage/src/environments/environment.ts');
 processEnvFile('../manage/src/environments/environment.prod.ts');
 
-// Open ../behavior/src/index.html
-let behaviorIndexHtml = readFileSync('../behavior/src/index.html').toString();
+function updateBaseRef(project: string, domain: string) {
+    // Open ../behavior/src/index.html
+    const indexFile = `../${project}/src/index.html`;
+    let indexHtml = readFileSync(indexFile).toString();
 
-// Replace <base id="baseHref" href="/" /> with the href being BEHAVIOR_DOMAIN
-behaviorIndexHtml = behaviorIndexHtml.replace(/<base id="baseHref" href="\/" \>/g, `<base id="baseHref" href="https://${process.env.BEHAVIOR_DOMAIN}/" >`);
+    // Replace <base id="baseHref" href="/" /> with the href being domain
+    indexHtml = indexHtml.replace(/<base id="baseHref" href="\/" \>/g, `<base id="baseHref" href="https://${domain}/" >`);
 
-// Write the update back to the original file
-writeFileSync('../behavior/src/index.html', behaviorIndexHtml);
+    // Write the 
+    // update back to the original file
+    writeFileSync(indexFile, indexHtml);
+
+}
+
+updateBaseRef('behavior', process.env.BEHAVIOR_DOMAIN);
+updateBaseRef('manage', process.env.MANAGE_DOMAIN);
